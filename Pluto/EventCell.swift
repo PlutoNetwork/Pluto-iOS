72d38e5aa3de5c3474ab8b7870a957226f09c305
Faisal M. Lalani <faisalmlalani@Faisals-MacBook-Pro.local>
2016-10-31 19:45:50 -0600

    Small bug fixes

--- Pluto/EventCell.swift
+++ Pluto/EventCell.swift
@@ -79,11 +79,11 @@ class EventCell: UITableViewCell {
             
             if let _ = snapshot.value as? NSNull {
                 
-                self.eventPlutoImageView.image = UIImage(named: "rocket-faded")
+                self.eventPlutoImageView.image = UIImage(named: "ship-faded")
                 
             } else {
                 
-                self.eventPlutoImageView.image = UIImage(named: "rocket")
+                self.eventPlutoImageView.image = UIImage(named: "ship-yellow")
             }
         })
     }
@@ -94,14 +94,14 @@ class EventCell: UITableViewCell {
             
             if let _ = snapshot.value as? NSNull {
                 
-                self.eventPlutoImageView.image = UIImage(named: "rocket")
+                self.eventPlutoImageView.image = UIImage(named: "ship-yellow")
                 self.event.adjustCount(addToCount: true)
                 self.userEventRef.setValue(true)
                 //self.syncToCalender(add: true)
                 
             } else {
                 
-                self.eventPlutoImageView.image = UIImage(named: "rocket-faded")
+                self.eventPlutoImageView.image = UIImage(named: "ship-faded")
                 self.event.adjustCount(addToCount: false)
                 self.userEventRef.removeValue()
                 //self.syncToCalender(add: false)
