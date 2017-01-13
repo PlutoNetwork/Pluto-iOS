e51f1cdcef0ee261e59c1e6945cadea55cbd5f29
Faisal M. Lalani <faisalmlalani@Faisals-MacBook-Pro.local>
2017-01-13 11:58:48 -0600

    Added nav bar to image controller

--- Pluto/ImageController.swift
+++ Pluto/ImageController.swift
@@ -19,26 +19,10 @@ class ImageController: UIViewController {
     var image = UIImage()
     
     // MARK: - VIEW
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-        /* Navigation bar customization */
-        self.navigationController?.setNavigationBarHidden(true, animated: true) // Keeps the navigation bar hidden.
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let rightGesture = UISwipeGestureRecognizer(target: self, action: #selector(ImageController.swipeRight))
-        rightGesture.direction = .right
-        imageView.addGestureRecognizer(rightGesture)
         
         imageView.image = image
     }
-    
-    func swipeRight(sender:UISwipeGestureRecognizer) {
-        
-        self.navigationController?.popViewController(animated: true)
-    }
-
 }
